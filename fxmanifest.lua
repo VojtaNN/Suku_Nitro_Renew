dd3239fca0f448483dd5b2237dc67cdad88fb770
VojtaNN <61760213+VojtaNN@users.noreply.github.com>
2022-09-28 14:33:03 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,14 @@
 fx_version 'cerulean'
 games { 'gta5' }
 lua54 'yes'
+
+
+--[[ Resource Information ]]--
+name         'Suku_Nitro_Renew'
+author       'barney_shit'
+version      '2.0.0'
+description  'Original made by barney_shit, fixed by VojtaN#5635 and Krupička#0263'
+
 shared_script '@ox_lib/init.lua'
               '@es_extended/imports.lua'
 
