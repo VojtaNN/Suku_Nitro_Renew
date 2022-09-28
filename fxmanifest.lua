074259d3d1bb3f8397c65c2a6cab2dd4a090aa13
VojtaNN <61760213+VojtaNN@users.noreply.github.com>
2022-09-28 20:43:11 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 --[[ Resource Information ]]--
 name         'Suku_Nitro_Renew'
 author       'barney_shit'
-version      '2.0.0'
+version      '2.1.0'
 description  'Original made by barney_shit, fixed by VojtaN#5635 and Krupička#0263'
 
 shared_script '@ox_lib/init.lua'
